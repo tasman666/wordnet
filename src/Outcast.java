dd5f90e8c09b81258e8ab811a01faf9bb0b059a9
tasman666 <a.trzepacz@gmail.com>
2014-04-14 23:06:31 +0200

    optimize calculating Outcast

--- src/Outcast.java
+++ src/Outcast.java
@@ -10,10 +10,13 @@ public class Outcast {
     public String outcast(String[] nouns) {
         String outcast = null;
         int maxDistance = 0;
+        Integer[][] distances = new Integer[nouns.length][nouns.length];
         for (int i = 0; i < nouns.length; i++) {
             int sumDistance = 0;
             for (int j = 0; j < nouns.length; j++) {
-                sumDistance += wordnet.distance(nouns[i], nouns[j]);
+                if (i != j) {
+                    sumDistance += getDistance(distances, nouns, i, j);
+                }
             }
             if (sumDistance > maxDistance) {
                 outcast = nouns[i];
@@ -21,6 +24,17 @@ public class Outcast {
             }
         }
         return outcast;
+    }
+
+    private int getDistance(Integer[][] distances, String[] nouns, int i, int j) {
+        if (distances[i][j] != null) {
+            return distances[i][j];
+        } else {
+            int distance = wordnet.distance(nouns[i], nouns[j]);
+            distances[i][j] = distance;
+            distances[j][i] = distance;
+            return distance;
+        }
     }
 
     // for unit testing of this class (such as the one below)
